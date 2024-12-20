683f45d1eb8f567aac532f7c0de18f0a6e6aad7a
Doğukan Gökduman <dogukangokdumann@gmail.com>
2024-12-21 02:29:15 +0300

    last

--- website/wsgi.py
+++
@@ -1,7 +0,0 @@
-import os
-from django.core.wsgi import get_wsgi_application
-
-# Doğru ayar modülünü belirtin: 'start.settings'
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'start.settings')
-
-application = get_wsgi_application()

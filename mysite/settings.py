a0524abf2b1c4683765f20953f7a2a5d0cf79676
Doğukan Gökduman <dogukangokdumann@gmail.com>
2024-12-27 17:08:10 +0300

    host

--- mysite/settings.py
+++ mysite/settings.py
@@ -33,7 +33,7 @@ STATICFILES_DIRS = [
 # Diğer ayarlar...
 
 DEBUG = False  # Geliştirme ortamında True, üretimde False olmalı
-ALLOWED_HOSTS = ['asit.onrender.com', 'localhost', '127.0.0.1','www.asit.com.tr']
+ALLOWED_HOSTS = ['asit.onrender.com', 'localhost', '127.0.0.1','www.asit.com.tr','https://asit-2dooe.ondigitalocean.app/','https://asit-2dooe.ondigitalocean.app']
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent

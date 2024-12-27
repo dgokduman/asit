6bc62cfeff16d813b6ad76a07246c4e5b37e10b0
Doğukan Gökduman <dogukangokdumann@gmail.com>
2024-12-27 21:09:05 +0300

    ss
    ss

--- start/urls.py
+++ start/urls.py
@@ -5,12 +5,12 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("", views.index, name="index"),  # Ana sayfa için URL
-    path("hakkinda/", views.about, name="about"),
-    path("kimyasallar/", views.chemicals, name="chemicals"),
+    path("hakkimizda/", views.about, name="about"),
+    path("urunlerimiz/", views.chemicals, name="chemicals"),
     path("ekibimiz/", views.team, name="team"),
     path("iletisim/", views.contact, name="contact"),
     path("siparis/", views.products, name="products"),
-    path("galeri/", views.gallery, name="gallery"),
+    path("fotograflar/", views.gallery, name="gallery"),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'start.views.view_404' 
